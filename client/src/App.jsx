ea743d994ba0fe26076079d0c414c1ffeb1a81c5
mabrar02 <mahdeen02@gmail.com>
2024-04-10 00:43:24 -0300

    restructured org of app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,7 @@
-import io from "socket.io-client";
-import { useEffect } from "react";
-const socket = io.connect("http://localhost:4000");
+import Lobby from "./components/Lobby";
 
 function App() {
-  const sendMessage = () => {
-    socket.emit("send_msg", { message: "Hello" });
-  };
-
-  useEffect(() => {
-    socket.on("r_msg", (data) => {
-      alert(data.message);
-    });
-  }, [socket]);
-
-  return (
-    <div>
-      <input placeholder="Message.." />
-      <button onClick={sendMessage}>Send Message</button>
-    </div>
-  );
+  return <Lobby />;
 }
 
 export default App;
---
+++ client/src/components/Lobby.jsx
@@ -0,0 +1,24 @@
+import io from "socket.io-client";
+import { useEffect } from "react";
+const socket = io.connect("http://localhost:4000");
+
+function Lobby() {
+  const sendMessage = () => {
+    socket.emit("send_msg", { message: "Hello" });
+  };
+
+  useEffect(() => {
+    socket.on("r_msg", (data) => {
+      alert(data.message);
+    });
+  }, [socket]);
+
+  return (
+    <div>
+      <input placeholder="Message.." />
+      <button onClick={sendMessage}>Send Message</button>
+    </div>
+  );
+}
+
+export default Lobby;
