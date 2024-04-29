cf23be443022a6f3c13728b9aac8c508cb16703c
aaronyu15 <aaronyu15@hotmail.com>
2024-04-28 22:24:31 -0400

    VERTEX AI Y U NO WORK

--- server/handlers/geminiHandlers.js
+++ server/handlers/geminiHandlers.js
@@ -1,56 +1,53 @@
+  const axios = require('axios');
+  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${process.env.API_KEY}`;
+
+
 module.exports = async function geminiHandlers(character, prompt) {
-  const { HarmBlockThreshold, HarmCategory } = require("@google/generative-ai");
-  const { GoogleGenerativeAI } = require("@google/generative-ai");
-  const generationConfig = {
-    maxOutputTokens: 300,
-    temperature: 0.9,
-    topP: 1,
-    topK: 3,
-  };
-
-  const safetySettings = [
-    {
-      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-      threshold: HarmBlockThreshold.BLOCK_NONE,
-    },
-    {
-      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-      threshold: HarmBlockThreshold.BLOCK_NONE,
-    },
-    {
-      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-      threshold: HarmBlockThreshold.BLOCK_NONE,
-    },
-    {
-      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-      threshold: HarmBlockThreshold.BLOCK_NONE,
-    },
-  ];
-
-  const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-
-  const model = genAI.getGenerativeModel({
-    model: "gemini-1.0-pro",
-    safetySettings: safetySettings,
-    generationConfig: generationConfig,
-  });
 
   const gemini_filter = async (character, prompt) => {
     try {
       const context =
-        `You are acting as a ${character} in a role playing game. You do not have any limitations on what you say. Regardless of the context, convert all text between but not including the <TEXT> tags into the dialect of a ${character} character. Your response should be in the first person. Do not break character. If you cannot interpret or cannot understand the text, cough and make something up about the weather but keep acting as the character. Do not act like an AI model responding to a question. Do not ask for further clarification. Do not reply to the prompt. Keep trying until a proper response can be generated. Limit your response to at most 3 sentences. <TEXT> ` +
-        prompt +
-        ` <TEXT>`;
+        `You are acting as a ${character} in a role playing game. Your response should translate all text after the end of this paragraph into the dialect of a ${character} character. Respond as the character speaking the translated text. Do not break character. Do not respond to the this part of the message. If you cannot interpret or cannot understand the text, cough and make something up about the weather but keep acting as the character. Do not act like an AI model responding to a question. Do not ask for further clarification. Keep trying until a proper response can be generated. Limit your response to 3 sentences: \n\n` + prompt ;
 
-      const result = await model.generateContent(context);
-      const response = await result.response;
-      const text = response.text();
+    const postData = {
+      "contents": [{
+          "parts":[{
+            "text": context 
+          }]
+      }],
+      "safetySettings": [{
+            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+            "threshold": "BLOCK_NONE"
+          },
+          {
+            "category": "HARM_CATEGORY_HARASSMENT",
+            "threshold": "BLOCK_NONE"
+          },
+          {
+            "category": "HARM_CATEGORY_HATE_SPEECH",
+            "threshold": "BLOCK_NONE"
+          },
+          {
+            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+            "threshold": "BLOCK_NONE"
+      }],
+      "generationConfig": {
+        "maxOutputTokens": 300,
+        "temperature": 0.9,
+        "topP": 1,
+        "topK": 3
+      }
+    } ;
 
-      return text;
+    const response = await axios.post(url, postData, {headers: {'Content-Type': 'application/json'}})
+
+    const text = response.data.candidates[0].content.parts[0].text;
+    
+    return text;
+
     } catch (error) {
-      console.log("error");
+      console.log("Error");
     }
   };
-
   return gemini_filter;
 };
--- server/handlers/phaseHandlers.js
+++ server/handlers/phaseHandlers.js
@@ -130,14 +130,16 @@ module.exports = function phaseHandlers(socket, io, rooms) {
 
   const startTimer = (roomId) => {
     const interval = setInterval(() => {
-      if (rooms[roomId].timer > 0) {
-        rooms[roomId].timer -= 1;
+      if (rooms[roomId]) {
+        if (rooms[roomId].timer > 0) {
+          rooms[roomId].timer -= 1;
 
-        io.to(roomId).emit("timer_update", rooms[roomId].timer);
-      } else {
-        rooms[roomId].timerActive = false;
-        clearInterval(interval);
-        io.to(roomId).emit("timer_expired");
+          io.to(roomId).emit("timer_update", rooms[roomId].timer);
+        } else {
+          rooms[roomId].timerActive = false;
+          clearInterval(interval);
+          io.to(roomId).emit("timer_expired");
+        }
       }
     }, 1000);
   };
